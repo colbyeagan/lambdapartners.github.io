19592146a42268202c8103ef0ba5cc1c9bb8fe92
ruivergani <ruiverganineto@gmail.com>
2023-05-23 12:28:07 +0100

    responsive section hero 991px completed

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,6 +1,37 @@
 // Script Data AOS Animation
 AOS.init();
 
+// Script for Hero Slide
+const progressSlide = document.querySelector('.js-progress'); // get progress bar
+const swiper = new Swiper(".slide-principal", {
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false // autoplay nao para de funcionar
+  },
+  effect: 'fade',
+  pagination: {
+    el: ".slide-principal .swiper-pagination",
+    clickable: true,
+    type: "fraction",
+  },
+  on: { // get some elements from swiper slide
+      init : function () { // when slide starts
+        progressSlide.classList.remove('animate');
+        progressSlide.classList.remove('active');
+        progressSlide.classList.add('animate'); // faz a barra crescer
+        progressSlide.classList.add('active'); // da opacidade
+      }, 
+      slideChangeTransitionStart : function (){ // when the transition start
+        progressSlide.classList.remove('animate');
+        progressSlide.classList.remove('active');
+        progressSlide.classList.add('active');
+      }, 
+      slideChangeTransitionEnd : function (){ // when the transiction finish
+        progressSlide.classList.add('animate'); // um apos o outro - nao pode adicionar junto
+      }
+    }
+});
+
 // Button Scroll Top
 const btnScrollTop = document.getElementById("js-btn-scroll-top");
 const imgBtnScrollTop = document.getElementById("js-img-footer");
@@ -39,5 +70,3 @@ if(header){
   }
   document.addEventListener("scroll", fixedMenu);
 }
-
-
