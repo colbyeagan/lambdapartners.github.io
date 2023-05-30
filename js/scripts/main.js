249eb990e43409ac7c71f8e5b350a3109cfbf6bc
ruivergani <ruiverganineto@gmail.com>
2023-05-30 16:17:23 +0100

    slide post details done

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -92,6 +92,29 @@ const swiperOtherProperties = new Swiper(".s-slide-other", {
   }
   
 });
+// Script for Swipper Slide
+const swiperSocialMedia = new Swiper(".slide-social-media", {
+  slidesPerView: 3,
+  spaceBetween: 32,
+  pagination: {
+    el: ".s-social-media__post .container .s-social-media__post__title .swiper-pagination",
+    clickable: true,
+  },
+  breakpoints:{
+    320 : {
+      slidesPerView: 1.3,
+      spaceBetween: 16,
+    },
+    600: {
+      slidesPerView: 2.3,
+      spaceBetween: 15,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    }
+  }
+});
 
 
 // Button Scroll Top
